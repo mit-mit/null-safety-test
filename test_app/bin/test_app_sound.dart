7a1e04973a791c0ac2c53bc95540abb633d01464
Michael Thomsen <mit@google.com>
2020-10-05 11:22:48 +0200

    Roll to Dart 2.10 deps

--- test_app/bin/test_app_sound.dart
+++ test_app/bin/test_app_sound.dart
@@ -1,5 +1,4 @@
 import 'package:test_pkg/test_pkg.dart' as pkg1;
-// import 'package:test_pkg_unmigrated/test_pkg_unmigrated.dart' as pkg2;
 
 void main(List<String> arguments) {
   // Non-nullable variable from migrated library function.
@@ -7,9 +6,6 @@ void main(List<String> arguments) {
 
   // Nullable variable from migrated library function.
   int? i1 = pkg1.nullableBar();
-
-  // Nullable variable from unmigrated library function.
-  // int i2 = pkg2.bar2();
 
   print('Hello: $i0, $i1');
 }
